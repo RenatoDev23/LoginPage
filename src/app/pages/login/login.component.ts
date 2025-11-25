5a03232e6eb57c3158975e6ec78aadba7e295af8
Renato <renatorenatex20@gmail.com>
2025-11-24 23:19:09 -0300

    criação dos components login

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../component/default-login-layout/default-login-layout.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [DefaultLoginLayoutComponent],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {}
